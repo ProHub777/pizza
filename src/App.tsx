d6537195974d948b359fddaad09841b844e8680c
Your Name <you@example.com>
2024-02-05 17:14:18 +0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Button from './Component/Button/Button';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button>Click</Button>
+    </>
+  );
 }
 
 export default App;
